9857f8bb982866b67824c248da3ccf24de621651
JonBon <guyguy@shaw.ca>
2019-05-31 17:19:46 -0700

    for python 3.7

--- Pause.py
+++ Pause.py
@@ -3,36 +3,36 @@ import time
 
 #must check whether the timer has expired
 def wait_for_pause(duration):
-   elapsed_time = time.clock()
-   print "You may pause the timer with 'p'"
-   pause = raw_input()
+   elapsed_time = time.perf_counter()
+   print ("You may pause the timer with 'p'")
+   pause = input()
    
    while(pause != 'p' and pause != 'P'):
-      print "Invalid; pause the timer with 'p'"
-      pause = raw_input()
-      elapsed_time = time.clock() - elapsed_time
+      print ("Invalid; pause the timer with 'p'")
+      pause = input()
+      elapsed_time = time.perf_counter() - elapsed_time
       remaining_time = duration - elapsed_time
       if(remaining_time <= 0):
-         print "Timer expired"
+         print ("Timer expired")
          return 0
 
    #probably doesn't need an 'if' statement
    if(pause == 'p' or pause == 'P'):
-      elapsed_time = time.clock() - elapsed_time
+      elapsed_time = time.perf_counter() - elapsed_time
       remaining_time = duration - elapsed_time
       if(remaining_time <= 0):
-         print "Timer expired"
+         print ("Timer expired")
          return 0
       else:
-         print "Time remaining: " + str(remaining_time)
+         print ("Time remaining: " + str(remaining_time))
          return remaining_time
    return 0
 
 def wait_for_unpause():
-   print "Unpause the timer with 'p'"
-   unpause = raw_input()
+   print ("Unpause the timer with 'p'")
+   unpause = input()
    while(unpause != 'p' and unpause != 'P'):
-      print "Invalid: Unpause the timer with 'p'"
+      print ("Invalid: Unpause the timer with 'p'")
       unpause = raw_input()
    if(unpause == 'p' or unpause == 'P'):
       return 0
