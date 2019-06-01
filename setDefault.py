a7d59a593826a9d1f9ef318d5535e0d38a7b47a4
JonBon <guyguy@shaw.ca>
2019-05-31 17:17:56 -0700

    for python 3.7

--- setDefault.py
+++ setDefault.py
@@ -1,11 +1,11 @@
-from Tkinter import *
-import Tkinter, Tkconstants, tkFileDialog
+from tkinter import *
+import tkinter, tkinter.constants, tkinter.filedialog
 
 
 def setDefault():
     root = Tk()
     root.filename = tkFileDialog.askopenfilename(initialdir = "C:/",title = "Select file",filetypes = (("mp3 files","*.mp3"),("wav files",".wav")))
-    print root.filename
+    print (root.filename)
 
     default = open("DEFAULT.txt", 'w')
     default.write(root.filename)
